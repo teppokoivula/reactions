4062e69787e37c64d8f855d90da5025db674ae08
Teppo Koivula <teppo.koivula@avoine.fi>
2024-04-22 08:32:46 +0300

    Add support for attributes for reaction buttons

--- Reactions.module.php
+++ Reactions.module.php
@@ -11,16 +11,19 @@ class Reactions extends WireData implements Module {
 		'like' => [
 			'title' => 'Like it',
 			'icon' => '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M720-120H280v-520l280-280 50 50q7 7 11.5 19t4.5 23v14l-44 174h258q32 0 56 24t24 56v80q0 7-2 15t-4 15L794-168q-9 20-30 34t-44 14Zm-360-80h360l120-280v-80H480l54-220-174 174v406Zm0-406v406-406Zm-80-34v80H160v360h120v80H80v-520h200Z"/></svg>',
+			'attrs' => [],
 			'sort' => 1,
 		],
 		'love' => [
 			'title' => 'Love it',
 			'icon' => '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/></svg>',
+			'attrs' => [],
 			'sort' => 2,
 		],
 		'haha' => [
 			'title' => 'Haha!',
 			'icon' => '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-260q68 0 123.5-38.5T684-400H276q25 63 80.5 101.5T480-260ZM312-520l44-42 42 42 42-42-84-86-86 86 42 42Zm250 0 42-42 44 42 42-42-86-86-84 86 42 42ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Z"/></svg>',
+			'attrs' => [],
 			'sort' => 3,
 		],
 	];
@@ -34,7 +37,7 @@ class Reactions extends WireData implements Module {
 		return [
 			'title' => 'Reactions',
 			'summary' => 'A module for collecting reactions on pages.',
-			'version' => '0.0.3',
+			'version' => '0.0.4',
 			'author' => 'Teppo Koivula',
 			'href' => 'https://github.com/teppokoivula/reactions',
 			'icon' => 'heart-o',
@@ -289,7 +292,18 @@ class Reactions extends WireData implements Module {
 					. '<span class="reaction-buttons__label">' . $this->sanitizer->entities1($reaction_data['title']) . '</span>';
 			}
 
-			$buttons[] = sprintf('<button class="reaction-buttons__button" data-page-id="%d" data-reaction="%s">%s</button>', $page ? $page->id : $this->page->id, $reaction_type, $button);
+			$attributes = '';
+			if (!empty($reaction_data['attrs'])) {
+				if (is_string($reaction_data['attrs'])) {
+					$attributes = ' ' . $reaction_data['attrs'];
+				} else if (is_array($reaction_data['attrs'])) {
+					foreach ($reaction_data['attrs'] as $attr => $value) {
+						$attributes .= ' ' . $this->sanitizer->entities1($attr) . '="' . $this->sanitizer->entities1($value) . '"';
+					}
+				}
+			}
+
+			$buttons[] = sprintf('<button class="reaction-buttons__button" data-page-id="%d" data-reaction="%s"%s>%s</button>', $page ? $page->id : $this->page->id, $reaction_type, $attributes, $button);
 		}
 
 		return sprintf('<div class="reaction-buttons">%s</div>', implode('', $buttons));
